e79b9b9c26bfb261725231cbbb53e884c9cb89f9
Shaun B <ashaun@ukho.gov.uk>
2026-01-14 16:32:43 +0000

    helper change

--- scripts/helm-package.ps1
+++ scripts/helm-package.ps1
@@ -1,25 +1,28 @@
-param(
-    [Parameter(Mandatory)]
-    [string]$ChartDirectory,
+function Invoke-HelmPackage {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]$ChartDirectory,
 
-    [Parameter(Mandatory)]
-    [string]$Version,
+        [Parameter(Mandatory)]
+        [string]$Version,
 
-    [string]$Destination
-)
+        [string]$Destination
+    )
 
-$cmd = @('package', $ChartDirectory)
-if ($PSBoundParameters.ContainsKey('Destination') -and $Destination) {
-    $cmd += @('--destination', $Destination)
+    $cmd = @('package', $ChartDirectory)
+    if ($PSBoundParameters.ContainsKey('Destination') -and $Destination) {
+        $cmd += @('--destination', $Destination)
+    }
+
+    $packageOutput = helm @cmd
+    Write-Host $packageOutput
+
+    if ($packageOutput -match ':\s+(.+\.tgz)$') {
+        return $matches[1]
+    }
+
+    $chartName = Split-Path $ChartDirectory -Leaf
+    $artifactPattern = if ($chartName) { "$chartName-$Version.tgz" } else { "*-$Version.tgz" }
+    return (Get-ChildItem -Path ($Destination ?? (Split-Path $ChartDirectory -Parent)) -Filter $artifactPattern | Select-Object -First 1).FullName
 }
-
-$packageOutput = helm @cmd
-Write-Host $packageOutput
-
-if ($packageOutput -match ':\s+(.+\.tgz)$') {
-    return $matches[1]
-}
-
-$chartName = Split-Path $ChartDirectory -Leaf
-$artifactPattern = if ($chartName) { "$chartName-$Version.tgz" } else { "*-$Version.tgz" }
-return (Get-ChildItem -Path ($Destination ?? (Split-Path $ChartDirectory -Parent)) -Filter $artifactPattern | Select-Object -First 1).FullName
